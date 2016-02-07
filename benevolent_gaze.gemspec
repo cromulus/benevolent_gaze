18af8b5f7a5bbf250cf6d1feba8ef49e0ed7fcb3
Bill Cromie <bill@cromie.org>
2016-02-07 14:33:50 -0500

    we are now responding to a bot! woot!

--- benevolent_gaze.gemspec
+++ benevolent_gaze.gemspec
@@ -31,6 +31,9 @@ Gem::Specification.new do |spec|
   spec.add_dependency "thor"
   spec.add_dependency "parallel"
   spec.add_dependency "slack-ruby-client"
+  spec.add_dependency "eventmachine"
+  spec.add_dependency "faye-websocket"
+  spec.add_dependency "celluloid-io"
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/benevolent_gaze/slacker.rb
+++ lib/benevolent_gaze/slacker.rb
@@ -1,8 +1,38 @@
 require 'json'
 require 'redis'
 require 'slack-ruby-client'
+require 'celluloid'
+require 'celluloid/io'
+Slack.configure do |config|
+  #config.token =  "xoxp-12422969797-12427127041-20531002689-15f0cd6035" #ENV['SLACK_API_TOKEN']
+  config.token = "xoxb-20159046901-w2V7crilpVg7Uxcr5fpFMdxl"
+end
 
 module BenevolentGaze
   class Slacker
+
+    def self.run!
+      client = Slack::RealTime::Client.new(websocket_ping: 60)
+      client.on :hello do
+        puts "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
+      end
+
+      client.on :message do |data|
+        #client.message channel: "#bot-testing", text:"Hi <@#{data'user']}>! #{data['text']}"
+        puts data['text']
+        case data['text']
+        when 'bot hi' then
+          client.message channel: "#{data['channel']}", text: "Hi <@#{data.user}>!"
+        when /^bot/ then
+          client.message channel: "#{data['channel']}", text: "Sorry <@#{data.user}>, what?"
+        end
+      end
+
+      client.start!
+    end
+
+    class << self
+      # where our methods go!
+    end
   end
 end
