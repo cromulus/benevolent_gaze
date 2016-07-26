df4330ddeae52384c58bf4cc41a4a1bf31c2ec87
Bill Cromie <bill@cromie.org>
2016-07-26 17:48:10 -0400

    more universal location for config info

--- lib/benevolent_gaze/kiosk.rb
+++ lib/benevolent_gaze/kiosk.rb
@@ -60,9 +60,8 @@ module BenevolentGaze
 
       OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
       APPLICATION_NAME = 'Blue Ridge Calendaring'
-      CLIENT_SECRETS_PATH = 'client_secret.json'
-      CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
-                                   "calendar-ruby-quickstart.yaml")
+      CLIENT_SECRETS_PATH = '/etc/bg/client_secret.json'
+      CREDENTIALS_PATH = File.join("/etc/bg/.credentials/calendar-ruby-quickstart.yaml")
       SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR
       CALENDAR_IDS = {
         'biggie' => 'robinhood.org_2d33313439373439322d363134@resource.calendar.google.com',
