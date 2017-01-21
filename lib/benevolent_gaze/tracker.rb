ac42919a76c6d878af3550ddfde4c5f9b10b18ec
Bill Cromie <bill@cromie.org>
2017-01-21 00:21:35 -0500

    minor update

--- lib/benevolent_gaze/tracker.rb
+++ lib/benevolent_gaze/tracker.rb
@@ -94,7 +94,8 @@ module BenevolentGaze
         # end
 
         begin
-          HTTParty.post("http://#{ENV['SERVER_HOST']}:#{ENV['IPORT']}/information", query: { devices: device_names_hash.to_json })
+          url = "http://#{ENV['SERVER_HOST']}:#{ENV['IPORT']}/information"
+          HTTParty.post(url, query: { devices: device_names_hash.to_json })
         rescue
           puts 'Looks like you might not have the Benevolent Gaze gem running'
         end
